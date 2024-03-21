dbba80deb41addd21257a26d5c95e79e1086ee2f
Explonentielle <126072458+Explonentielle@users.noreply.github.com>
2024-03-21 18:16:41 +0100

    link linkedin and github added bis

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -16,7 +16,7 @@ export const socials = [
     },
     {
         name: 'ADBdevelopement@gmail.com',
-        link: '/mailto:aklex59debonnieres@gmail.com:',
+        link: '/mailto:alex59debonnieres@gmail.com:',
         path: 'M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z',
     },
 ];
